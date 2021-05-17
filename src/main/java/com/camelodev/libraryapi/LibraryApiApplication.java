52e159edb9202ee65040c6282830ebd3a5424536
daniel.nascimento <daniel.nascimento@seap.ma.gov.br>
2021-05-17 17:19:10 -0300

    Concluindo a implementação do serviço para envio de emails

--- src/main/java/com/camelodev/libraryapi/LibraryApiApplication.java
+++ src/main/java/com/camelodev/libraryapi/LibraryApiApplication.java
@@ -1,19 +1,37 @@
 package com.camelodev.libraryapi;
 
+import com.camelodev.libraryapi.service.EmailService;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
+import java.util.Arrays;
+import java.util.List;
+
 @SpringBootApplication
 @EnableScheduling
 public class LibraryApiApplication {
 
+    @Autowired
+    private EmailService emailService;
+
     @Bean
     public ModelMapper modelMapper(){
         return new ModelMapper();
+    }
+
+    @Bean
+    public CommandLineRunner runner(){
+        return args -> {
+            List<String> emails = Arrays.asList("library-api-3b19a4@inbox.mailtrap.io");
+            emailService.sendMails("Testando serviço de emails.", emails);
+            System.out.println("EMAILS ENVIADOS");
+        };
     }
 
     public static void main(String[] args) {
