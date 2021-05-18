c7dfc57bdd0ab8a70d2a7d6bfc8e52a7156bc326
daniel.nascimento <daniel.nascimento@seap.ma.gov.br>
2021-05-18 10:32:06 -0300

    Finalizando os módulos da aplicação

--- src/main/java/com/camelodev/libraryapi/LibraryApiApplication.java
+++ src/main/java/com/camelodev/libraryapi/LibraryApiApplication.java
@@ -17,21 +17,9 @@ import java.util.List;
 @EnableScheduling
 public class LibraryApiApplication {
 
-    @Autowired
-    private EmailService emailService;
-
     @Bean
     public ModelMapper modelMapper(){
         return new ModelMapper();
-    }
-
-    @Bean
-    public CommandLineRunner runner(){
-        return args -> {
-            List<String> emails = Arrays.asList("library-api-3b19a4@inbox.mailtrap.io");
-            emailService.sendMails("Testando serviço de emails.", emails);
-            System.out.println("EMAILS ENVIADOS");
-        };
     }
 
     public static void main(String[] args) {
